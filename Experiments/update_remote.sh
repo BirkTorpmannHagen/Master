4985e9c1b508fe41333799b6730310053e23105e
bstorpma <bstorpma@uio.no>
2021-11-11 12:03:29 +0100

    fixed script?

--- Experiments/update_remote.sh
+++ Experiments/update_remote.sh
@@ -1,3 +1,3 @@
 #Tunnels into cluster, pulls latest changes, adds and commits any new data, then exits.
-ssh -tt -AY2C bstorpma@dnat.simula.no -p 60441 "ls; rlogin g001; cd Master; ls;
+ssh -tt -AY2C bstorpma@dnat.simula.no -p 60441 "ls; cd Master; ls;
  git pull; ls; exit"
