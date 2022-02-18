24d4981676d28f3e8edebb57080110bac4464445
BirkTorpmannHagen <birk.sebastian.torpmann@gmail.com>
2022-02-18 10:45:59 +0100

    unet

--- Experiments/train_augmented_pipeline.py
+++ Experiments/train_augmented_pipeline.py
@@ -2,7 +2,7 @@ from training.consistency_trainers import *
 import sys
 
 if __name__ == '__main__':
-    config = {"model": "DeepLab",
+    config = {"model": "Unet",
               "device": "cuda",
               "lr": 0.00001,
               "batch_size": 8,
@@ -11,23 +11,35 @@ if __name__ == '__main__':
     # config["model"] = sys.argv[2]
     trainer = ConsistencyTrainer(id=f"{sys.argv[1]}", config=config)
     trainer.train()
-    # config = {"model": "DeepLab",
-    #           "device": "cuda",
-    #           "lr": 0.00001,
-    #           "batch_size": 8,
-    #           "epochs": 250,
-    #           "use_inpainter": False}
-    # trainer = StrictConsistencyTrainer(id=f"dual_jaccard-{sys.argv[1]}", config=config)
-    # trainer.train()
-    # trainer = ConsistencyTrainerUsingAugmentation(id=f"augmentation-{sys.argv[1]}", config=config)
-    # trainer.train()
-    # trainer = ConsistencyTrainerUsingControlledAugmentation("aug_test", config)
-    # trainer.train()
-    # config = {"model": "DeepLab",
-    #           "device": "cuda",
-    #           "lr": 0.00001,
-    #           "batch_size": 8,
-    #           "epochs": 250,
-    #           "use_inpainter": False}
+    config = {"model": "Unet",
+              "device": "cuda",
+              "lr": 0.00001,
+              "batch_size": 8,
+              "epochs": 250,
+              "use_inpainter": False}
+    trainer = StrictConsistencyTrainer(id=f"dual_jaccard-{sys.argv[1]}", config=config)
+    trainer.train()
+    config = {"model": "Unet",
+              "device": "cuda",
+              "lr": 0.00001,
+              "batch_size": 8,
+              "epochs": 250,
+              "use_inpainter": False}
+    trainer = ConsistencyTrainerUsingAugmentation(id=f"augmentation-{sys.argv[1]}", config=config)
+    trainer.train()
+    config = {"model": "Unet",
+              "device": "cuda",
+              "lr": 0.00001,
+              "batch_size": 8,
+              "epochs": 250,
+              "use_inpainter": False}
+    trainer = ConsistencyTrainerUsingControlledAugmentation("aug_test", config)
+    trainer.train()
+    config = {"model": "Unet",
+              "device": "cuda",
+              "lr": 0.00001,
+              "batch_size": 8,
+              "epochs": 250,
+              "use_inpainter": False}
     # trainer = AdversarialConsistencyTrainer(id=f"adversarial-{sys.argv[1]}", config=config)
     # trainer.train()
