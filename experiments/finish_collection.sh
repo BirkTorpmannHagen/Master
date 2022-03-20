9a459a7d821271d9e4594c991a43e0ea99e2622a
BirkTorpmannHagen <birk.sebastian.torpmann@gmail.com>
2022-03-20 16:31:18 +0100

    Collection scripts

--- experiments/finish_collection.sh
+++ experiments/finish_collection.sh
@@ -1,4 +1,4 @@
 export EXPERIMENT_MODEL="DeepLab"
 sbatch --array=[1-10%2] Experiments/enqueue_all.sbatch
 export EXPERIMENT_MODEL="TriUnet"
-sbatch --array=[5-10%2] Experiments/enqueue_all.sbatch
+sbatch --array=[5-10%1] Experiments/enqueue_all.sbatch
